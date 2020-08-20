75d971c423f6c3dac0a2f7dbbc07a38fc4abd1b5
Diego Oliveira <diegomaka@gmail.com>
2020-08-20 19:19:43 -0300

    First routes controllers and views created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, except: %i[destroy edit update] do
+    resources :doses, only: %i[new create]
+  end
+
+  resources :doses, only: :destroy
+
+  root to: 'cocktails#index'
 end
